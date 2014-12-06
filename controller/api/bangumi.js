df054f85cdb3b168771a5315328facc68b548c09
Phoenix Nemo <i@phoenixlzx.com>
2014-12-06 22:41:12 +0800

    bangumi api routes

--- controller/api/bangumi.js
+++ controller/api/bangumi.js
@@ -28,10 +28,7 @@ module.exports = function (api) {
 
     api.post('/bangumi/add', function *(next) {
         var body = this.request.body;
-        if (validator.isDate(body.startDate) &&
-            validator.isDate(body.endDate) &&
-            validator.isInt(body.showOn) &&
-            body.name && body.tag) {
+        if (isValid(body)) {
             var bangumi = new Bangumis({
                 name: body.name,
                 startDate: body.startDate,
@@ -45,6 +42,46 @@ module.exports = function (api) {
                 return;
             }
         }
-        this.body = {success: false};
+        this.body = { success: false };
     });
+
+    api.post('/bangumi/update', function *(next) {
+        var body = this.request.body;
+        if (isValid(body)) {
+            var bangumi = new Bangumis({
+                name: body.name,
+                startDate: body.startDate,
+                endDate: body.endDate,
+                showOn: body.showOn,
+                tag: body.tag
+            });
+            var b = yield bangumi.update();
+            if (b) {
+                this.body = { success: true };
+                return;
+            }
+        }
+    });
+
+    api.post('/bangumi/remove', function *(next) {
+        var body = this.request.body;
+        yield new Bangumis().remove(body._id);
+        return this.body = { success: true };
+    });
+};
+
+var isValid = function(bangumi) {
+    if (validator.isDate(bangumi.startDate) && validator.isDate(bangumi.endDate)) {
+        return false;
+    }
+    if ([1, 2, 3, 4, 5, 6, 7].indexOf(bangumi.showOn) === -1) {
+        return false;
+    }
+    if (typeof bangumi.name !== 'string' || !bangumi.name) {
+        return false;
+    }
+    if (bangumi.tag.length !== 24 || !bangumi.tag) {
+        return false;
+    }
+    return true;
 };
