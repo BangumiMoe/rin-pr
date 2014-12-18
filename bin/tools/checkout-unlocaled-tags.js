b788fa5426f9ab4dc8920c36be9c8b2eada6e410
tengattack <tengattack@foxmail.com>
2014-12-18 20:59:25 +0800

    update tools

--- bin/tools/checkout-unlocaled-tags.js
+++ bin/tools/checkout-unlocaled-tags.js
@@ -1,7 +1,26 @@
-var f = function(tags) {
-    tags.forEach(function(t) {
-        if (!t.locale && t.type === 'bangumi') {
-            console.log(t.name);
+
+var co = require('./../../node_modules/koa/node_modules/co');
+var config = require('./../../config');
+var models = require('./../../models'),
+    Tags = models.Tags;
+
+var main = function *() {
+    var tags = yield new Tags().getAll();
+    tags.forEach(function(tag) {
+        if (!tag.locale || !tag.locale[0]) {
+            console.log(tag.name);
         }
     });
 };
+
+function onerror(err) {
+  if (err) {
+    console.error(err.stack);
+  }
+}
+
+setTimeout(function () {
+  var ctx = new Object();
+  var fn = co(main);
+  fn.call(ctx, onerror);
+}, 800);
