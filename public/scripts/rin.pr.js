48277b5e70718784e6bd864450605d02983c0254
Phoenix Nemo <i@phoenixlzx.com>
2014-12-21 19:11:41 +0800

    rebuilt js

--- public/scripts/rin.pr.js
+++ public/scripts/rin.pr.js
@@ -6103,7 +6103,7 @@ var rin = angular.module('rin', [
             $scope.downloadTorrent = function(torrent) {
                 torrent.downloads += 1;
                 var downloadLink = '/download/torrent/' + torrent._id +
-                    '/' + torrent.title + '.torrent';
+                    '/' + torrent.title.replace(/\//g, '_') + '.torrent';
                 var urlCreator = $window.URL || $window.webkitURL || $window.mozURL || $window.msURL;
                 var link = document.createElement("a");
                 if (urlCreator && "download" in link) {
