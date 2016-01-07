a4977d932d09f25736bf3f180a3d368b87fa680c
Phoenix Nemo <i@phoenixlzx.com>
2016-01-07 16:37:22 +0800

    fix function

--- bin/tools/import_bangumi_acgdb.js
+++ bin/tools/import_bangumi_acgdb.js
@@ -248,7 +248,8 @@ var acgdb_parse = function*(data) {
 
                 // default to 12 episodes / increase 12 weeks / 82 days beased on startDate
                 var endDate = new Date(ani.bangumi.startDate);
-                ani.bangumi.endDate = endDate.setDate(endDate.getDate() + 82).getTime();
+                endDate.setDate(endDate.getDate() + 82);
+                ani.bangumi.endDate = endDate.getTime();
 
                 ani.tag.type = 'bangumi';
 
