6c83c82a658282657dec84110723f6bbd487fd10
tengattack <tengattack@foxmail.com>
2017-05-28 12:55:43 +0800

    Update xss a href support protocols

--- test/lib.js
+++ test/lib.js
@@ -21,4 +21,14 @@ describe("xss", function() {
   it("attr 'onload' should be remove", function() {
     xss('<img src="/test.jpg" onload="alert(\'test\')"/>').should.eql("<img src=\"/test.jpg\" />");
   });
+  it("<a> attr 'href' should be kept", function() {
+    var links = [
+      '<a href="https://www.example.com">example</a>',
+      '<a href="ed2k://|file|eMule0.49c.zip|2868871|0F88EEFA9D8AD3F43DABAC9982D2450C|/">ed2k link</a>',
+      '<a href="magnet:?xt=urn:btih:c12fe1c06bba254a9dc9f519b335aa7c1367a88a">magnet link</a>',
+    ];
+    for (var i = 0; i < links.length; i++) {
+      xss(links[i]).should.eql(links[i]);
+    }
+  });
 });
