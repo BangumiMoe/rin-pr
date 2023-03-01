86c2b8d85011b7456a68f03c540e970d5aa3c378
shirou <shirou@bangumi.moe>
2023-03-01 14:16:35 +0000

    Update health check mongodb

--- controller/health/index.js
+++ controller/health/index.js
@@ -8,7 +8,13 @@
 var Router = require('koa-router');
 var api = new Router();
 
+var Models = require('./../../models'),
+  Torrents = Models.Torrents;
+
 api.get('', function *(next) {
+  var torrent = new Torrents();
+  yield torrent.collection.find().sort({ _id: -1 }).limit(1).toArray();
+
   this.body = {success: true};
 });
 
